82d9032011bedcb4df5ff1007a43e71ce3a242f9
Kallef Alexandre de Souza <kallef@Kallefs-MBP.agencia110>
2015-07-02 12:56:19 -0300

    initial structure

---
+++ src/Phreal.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Prhost\Phreal;
+
+class Phreal
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     *
+     * @param string $phrase Phrase to return
+     * @return string Returns the phrase passed in
+     */
+    public function run($phrase)
+    {
+        return $phrase;
+    }
+}
