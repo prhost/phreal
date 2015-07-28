1fac6e43ea6f33222b65cb95fc7d35dba1f7f01a
Kallef Alexandre de Souza <kallef@Kallefs-MBP.agencia110>
2015-07-28 13:26:29 -0300

    rodando normal agora

--- src/Messages/Message.php
+++ src/Messages/Message.php
@@ -1,67 +1,10 @@
 <?php namespace Prhost\Phreal\Messages;
 
-use Prhost\Phreal\Exceptions\InvalidMessageFormat;
-
 /**
  * Class Message
  * @package Prhost\Phreal\Messages
  */
 class Message
 {
-    private $destination = [
-        'eventCall'  => null,
-        'eventAlias' => null
-    ];
 
-    private $data = [];
-
-    public function __construct($message)
-    {
-        $messageRaw = json_decode($message);
-
-        InvalidMessageFormat::validateJson();
-
-        $this->populate($messageRaw);
-    }
-
-    /**
-     * @param \stdClass $messageRaw
-     */
-    public function populate(\stdClass $messageRaw)
-    {
-        $this->setEvent($messageRaw);
-        $this->setData($messageRaw);
-    }
-
-    /**
-     * @param \stdClass $message
-     * @throws \Exception
-     */
-    private function setEvent(\stdClass $message)
-    {
-        if ($message->event) {
-            $event = $message->event;
-            $this->destination['eventAlias'] = $event;
-        } else {
-            throw new \Exception('Event required', 400);
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public function getData()
-    {
-        return $this->data;
-    }
-
-    /**
-     * @param \stdClass $message
-     */
-    public function setData(\stdClass $message)
-    {
-        if (isset($message->data)) {
-            $this->data = $message->data;
-        }
-    }
 }
